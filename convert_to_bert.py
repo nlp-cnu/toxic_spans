068139437a0c8b998fa246dd4d353579b6321ca4
rafael.diaz5512@gmail.com <rafael.diazcruz.19@cnu.edu>
2020-11-12 20:20:26 -0500

    added framework for a function

--- convert_to_bert.py
+++ convert_to_bert.py
@@ -4,5 +4,14 @@
 @date 11/12/2020
 """
 
+
+def read_spans(read_file):
+    """
+
+    :param read_file:
+    :return: not sure yet
+    """
+
+
 if __name__ == "__main__":
     print("Placeholder")
